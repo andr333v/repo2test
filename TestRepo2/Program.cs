87325bf153592e79193b64187ce785bc614799ac
andreyandreev-tlrk <andrey.andreev@telerik.com>
2015-06-26 16:16:17 +0300

    first commit in test repo 2

---
+++ TestRepo2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRepo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Repo 2 Works !!!");
+        }
+    }
+}
